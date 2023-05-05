0c30709b9750401d46041e8f4c93a68a2e6f4753
Ankush <ankushnitjsr@gmail.com>
2023-05-05 19:41:15 +0530

    Add service and repository layers

--- src/main/java/com/driver/controllers/HotelManagementservice.java
+++ src/main/java/com/driver/controllers/HotelManagementservice.java
@@ -1,9 +1,13 @@
 package com.driver.controllers;
 
+import com.driver.model.Booking;
+import com.driver.model.Facility;
 import com.driver.model.Hotel;
 import com.driver.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class HotelManagementservice {
@@ -15,5 +19,21 @@ public class HotelManagementservice {
 
     public Integer addUser(User user) {
         return hotelManagementRepository.saveUser(user);
+    }
+
+    public String getHotelWithMostFacilities() {
+        return hotelManagementRepository.hotelWithMostFacilities();
+    }
+
+    public int bookARoom(Booking booking) {
+        return hotelManagementRepository.roomBooking(booking);
+    }
+
+    public int getBookingByAPerson(Integer aadharCard) {
+        return hotelManagementRepository.numberOfBookingByAPerson(aadharCard);
+    }
+
+    public Hotel updateFacilities(List<Facility> newFacilities, String hotelName) {
+        return hotelManagementRepository.updateNewFacilities(newFacilities, hotelName);
     }
 }
